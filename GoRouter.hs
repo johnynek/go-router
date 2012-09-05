6e3318a4b0d4ed1401bb48bf51026bb0ce4ad574
Oscar Boykin <oscar@twitter.com>
2012-09-04 23:08:31 -0700

    Minor cleanups

--- GoRouter.hs
+++ GoRouter.hs
@@ -5,19 +5,16 @@ import Data.Maybe
 -- | 'main' runs the main program
 main :: IO ()
 main = do
+  (route:rest) <- getArgs
   homeDir <- getEnv "HOME"
   fileData <- readFile (homeDir ++ "/.go-routes")
   let rts = getRoutes (lines fileData)
-  (route:rest) <- getArgs
   let cmd = bashCmd rts route
   putStrLn cmd
 
-getRoute :: String -> (String, String)
-getRoute line
-  = let (a, b) = (break (== ' ') line)
-    in (a, tail b)
-
 getRoutes lines = Map.fromList $ map getRoute lines
+  where getRoute line = let (a, b) = (break (== ' ') line)
+                        in (a, tail b)
 
 bashCmd :: Map.Map String String -> String -> String
 bashCmd routes cmd = fromMaybe ("open http://go/" ++ cmd) $ Map.lookup cmd routes
